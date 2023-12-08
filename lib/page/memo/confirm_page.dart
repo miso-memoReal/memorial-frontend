6a307e3f1146030eee35e191b2dca85d41a33e1d
Takizawariku <86756539+Takizawariku@users.noreply.github.com>
2023-12-08 11:44:28 +0900

    add 背景ブラー

--- lib/page/memo/confirm_page.dart
+++ lib/page/memo/confirm_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class ConfirmSendArguments {
@@ -8,35 +10,73 @@ class ConfirmSendArguments {
 
 class MemoConfirm extends StatelessWidget {
   const MemoConfirm({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final args =
         ModalRoute.of(context)!.settings.arguments as ConfirmSendArguments;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('memoconfirm'),
-      ),
-      body: Column(
-        children: [
-          const Text("メモ内容"),
-          Text(args.str),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              // primary: Colors.blue,
-              // onPrimary: Colors.black,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-            onPressed: () {
-              Navigator.pushNamed(context, "/");
-            },
-            child: const Center(child: Text('作成')),
+        body: Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(
+          color: Colors.blue.withOpacity(0.1),
+        ),
+        BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+          child: Container(
+            color: Colors.transparent,
           ),
-        ],
-      ),
-    );
-    // TODO: implement build
+        ),
+        Scaffold(
+          floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.pushNamed(context, "/memoCreate");
+            },
+            backgroundColor: Colors.transparent,
+            elevation: 0, // 影を表示しない
+            shape: RoundedRectangleBorder(
+              side: BorderSide(color: Colors.white, width: 2.0),
+              borderRadius: BorderRadius.circular(50), // 円形の形状にする
+            ),
+            // shape: CircleBorder(side: Colors.black87),
+            child: Image.asset(
+              'lib/assets/images/backicon.png', // イメージのパス
+              // width: 24, // 画像のサイズを指定
+              // height: 24,
+            ),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "メモ内容",
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  args.str,
+                  style: TextStyle(fontSize: 16),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF5FC569),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/cameraPage");
+                  },
+                  child: const Text("メモを作成", style: TextStyle(fontSize: 16)),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
--- lib/page/memo/create_page.dart
+++ lib/page/memo/create_page.dart
@@ -1,38 +1,101 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+
 import 'confirm_page.dart';
 
 class MemoCreate extends StatefulWidget {
-  const MemoCreate({super.key});
+  const MemoCreate({Key? key}) : super(key: key);
 
   @override
   MemoCreateState createState() => MemoCreateState();
 }
 
 class MemoCreateState extends State<MemoCreate> {
-  String argtext = ''; // メモの内容を保持する変数
+  String argtext = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('memocreate'),
-      ),
-      body: Column(
+      body: Stack(
+        fit: StackFit.expand,
         children: [
-          const Text("メモ内容"),
-          TextField(
-            onChanged: (value) {
-              setState(() {
-                argtext = value; // テキストの変更をargtextに保存
-              });
-            },
+          // 透明度が99%の白い背景
+          Container(
+            color: Colors.blue.withOpacity(0.1),
           ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, "/memoConfirm",
-                  arguments: ConfirmSendArguments(str: argtext));
-            },
-            child: const Text('確認へ進む'),
+          // 透明な背景とブラー効果
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+            child: Container(
+              color: Colors.transparent, // 透明な背景色
+            ),
+          ),
+          Scaffold(
+            backgroundColor: Colors.transparent, // Scaffoldの背景を透明に
+            floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/cameraPage");
+              },
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(color: Colors.white, width: 2.0),
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Image.asset(
+                'lib/assets/images/backicon.png',
+              ),
+            ),
+            body: Column(
+              children: [
+                const SizedBox(height: 100),
+                const Text(
+                  "メモ内容",
+                  style: TextStyle(fontSize: 18),
+                ),
+                Expanded(
+                  child: TextField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: 5,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Color(0xFF5FC569)),
+                      ),
+                      filled: true,
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        argtext = value;
+                      });
+                    },
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF5FC569),
+                    minimumSize: const Size(198, 56),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(
+                      context,
+                      "/memoConfirm",
+                      arguments: ConfirmSendArguments(str: argtext),
+                    );
+                  },
+                  child: const Text(
+                    '確認へ進む →',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                const SizedBox(height: 400),
+              ],
+            ),
           ),
         ],
       ),
