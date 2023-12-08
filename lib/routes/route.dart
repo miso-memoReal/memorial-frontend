5099618ce42caf5fc7cf5ad212d3028d89cea82b
Takizawariku <86756539+Takizawariku@users.noreply.github.com>
2023-12-08 11:40:51 +0900

    add route

--- lib/routes/route.dart
+++ lib/routes/route.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:memoreal/page/camera_page.dart';
 import 'package:memoreal/page/memo/confirm_page.dart';
 import 'package:memoreal/page/memo/create_page.dart';
 
 final Map<String, WidgetBuilder> routes = {
+  '/cameraPage': (context) => const CameraPage(),
   '/memoCreate': (context) => const MemoCreate(),
   '/memoConfirm': (context) => const MemoConfirm(),
   // 他の画面のルートをここに追加
